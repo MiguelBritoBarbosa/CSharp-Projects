7210aaf797ed71b43cd00f4bce6c09edb5c01796
Miguel Brito Barbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2022-10-19 17:36:29 -0300

    Projeto 3° Bimestre
    Projeto de PVB do 3° Bimestre

---
+++ Projeto3°Bimestre/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Projeto3_Bimestre
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selecione a data da compra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite o valor da compra";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(318, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Digite a quantidade de parcelas";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 183);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(247, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Fatura";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(20, 65);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(273, 23);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(336, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(290, 199);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(336, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total a pagar:";
+            this.label4.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(10, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 45);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Pagar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(10, 342);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(10, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(320, 30);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Selecione a data do pagamento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 459);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Projeto3°Bimestre/Form1.cs
@@ -0,0 +1,137 @@
+﻿/*Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ *Curso Técnico em Informática - Data de Entrega: 06 / 09 / 2022
+ * Autores do Projeto: Miguel Brito Barbosa - Lucas de Oliveira Maia
+ *
+ * Turma: 2M
+ * Atividade Proposta em aula
+ * Observação: 
+    dateTimePicker1: controle de data onde o usuário irá inserir o dia da compra feita.
+    textBox1: caixa de texto onde o usuário irá inserir o valor da compra.
+    textBox2: caixa de texto que receberá a quantidade de parcelas da compra.
+    button1: botão que gera as parcelas e seus valores e o enviam para uma listbox.
+    listBox1: caixa de lista que receberá as parcelas geradas pelo botão.
+    label4: texto que receberá o valor total da compra.
+    dateTimePicker2: controle de data onde o usuário irá inserir o dia do pagamento.
+    button2: botão que pagará uma parcela selecionada na listbox1 pelo usuário.
+ * 
+ * 
+ * ******************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto3_Bimestre
+{
+   public partial class Form1 : Form
+    {
+
+        float ValorMensal;
+        float Valor;
+        int Parcelas;
+        DateTime[] Datas = new DateTime[0];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Datas.Length == 0)
+            {
+                DateTime DataCompra = dateTimePicker1.Value.Date;
+                if (float.TryParse(textBox1.Text, out Valor) && int.TryParse(textBox2.Text, out Parcelas))
+                {
+                    Valor = float.Parse(textBox1.Text);
+                    Parcelas = int.Parse(textBox2.Text);
+                    ValorMensal = Valor / Parcelas;
+
+                    label4.Text = string.Format($"Total a pagar: {Valor:C2}");
+                    Array.Resize(ref Datas, Datas.Length + Parcelas);
+
+                    for (int i = 0; i < Parcelas; i++)
+                    {
+
+                        DateTime DataParcela = DataCompra.AddMonths(i);
+
+                        int diaSemana = (int)DataParcela.DayOfWeek;
+
+                        if (diaSemana == 0)
+                        {
+                            DataParcela = DataParcela.AddDays(1);
+                        }
+                        else if (diaSemana == 6)
+                        {
+                            DataParcela = DataParcela.AddDays(2);
+                        }
+
+
+                        listBox1.Items.Add(String.Format($"{i + 1}° Parcela: {ValorMensal:C2} - " + DataParcela.ToString("dd/MM/yyyy")));
+                        Datas[i] = DataParcela;
+                    }
+                    label4.Visible = true;
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Digite os valores corretamente!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pagamento de compra peendente. Termine o pagamento para realizar outra compra");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Datas.Length == 1)
+            {
+                button2.Enabled = false;
+            }
+            DateTime DataPagamento = dateTimePicker2.Value.Date;
+
+            int indice = listBox1.SelectedIndex;
+
+            if ( indice != -1 && DateTime.Compare(Datas[indice], DataPagamento) < 0)
+            {
+                MessageBox.Show(String.Format($"Pagamento atrasado. Valor da parcela reajustado: {ValorMensal*1.03:C2}"));
+            }
+
+            //MessageBox.Show(Datas[indice].ToString() + " - " + DataPagamento.ToString());
+
+            int Id = listBox1.SelectedIndex;
+            if (Id == -1)
+            {
+                MessageBox.Show("Selecione uma parcela!");
+            }
+            else if (Id != 0)
+            {
+                MessageBox.Show("Não foi possivel pagar está parcela pois há uma parcela anterior peendente");
+            }
+            else
+            {
+                listBox1.Items.RemoveAt(Id);
+                Valor -= ValorMensal;
+                label4.Text = string.Format($"Total a pagar: {Valor:C2}");
+                if (Datas.Length >= 1)
+                {
+                    for (int i = 0; i < Datas.Length - 1; i++)
+                    {
+                        Datas[i] = Datas[i + 1];
+                    }
+                    Array.Resize(ref Datas, Datas.Length - 1);
+                }
+            }
+        }
+    }
+}
