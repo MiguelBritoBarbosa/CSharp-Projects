c91d47c17947b2cedc56a0d97cffa33e7cef8061
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2023-08-25 20:53:35 -0300

    Projeto 3° Bim
    Finalizando projeto 3° bimestre

--- Projeto3Bim/Form1.Designer.cs
+++ Projeto3Bim/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Variável de designer necessária.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpar os recursos que estão sendo usados.
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,83 +20,106 @@
             base.Dispose(disposing);
         }
 
-        #region Código gerado pelo Windows Form Designer
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necessário para suporte ao Designer - não modifique 
-        /// o conteúdo deste método com o editor de código.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(42, 78);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(319, 165);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(367, 78);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(319, 165);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(692, 78);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(319, 165);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button1.Location = new System.Drawing.Point(451, 474);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 87);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Anchor = AnchorStyles.Bottom;
+            button1.Location = new Point(539, 559);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 82);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(394, 335);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Dock = DockStyle.Fill;
+            pictureBox2.Location = new Point(403, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(394, 335);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Dock = DockStyle.Fill;
+            pictureBox3.Location = new Point(803, 3);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(395, 335);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(pictureBox3, 2, 0);
+            tableLayoutPanel1.Controls.Add(pictureBox2, 1, 0);
+            tableLayoutPanel1.Location = new Point(12, 30);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1201, 341);
+            tableLayoutPanel1.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1051, 605);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1225, 653);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Button button1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
-
--- Projeto3Bim/Form1.cs
+++ Projeto3Bim/Form1.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 
 namespace Projeto3Bim
 {
@@ -14,7 +8,7 @@ namespace Projeto3Bim
     {
         Bitmap imgFogao = new Bitmap("C:/Imagens/Imagem_A.jpg");
         Bitmap imgPanela = new Bitmap("C:/Imagens/Panela.jpg");
-        Bitmap imgnova = new Bitmap("C:/Imagens/Imagem_A.jpg"   );
+        Bitmap imgnova = new Bitmap("C:/Imagens/Imagem_A.jpg");
 
 
         public Form1()
@@ -28,23 +22,52 @@ namespace Projeto3Bim
         }
 
 
-        public Color getPanela(byte x, byte y, byte r, byte g, byte b )
+        public void BinareImage()
         {
-            Bitmap imgPanela = new Bitmap("C:/Imagens/Panela.jpg");
-            byte rP = imgPanela.GetPixel(x, y).R;
-            byte gP = imgPanela.GetPixel(x, y).G;
-            byte bP = imgPanela.GetPixel(x, y).B;
-            if (!(r == 255 & g == 255 && b == 0))
+            int coluna = imgnova.Width;
+            int linha = imgnova.Height;
+            Color cor;
+            for (int i = 0; i <= coluna - 1; i++)
             {
-                return imgPanela.GetPixel(x, y);
+                for (int j = 0; j <= linha - 1; j++)
+                {
+                    byte r = imgFogao.GetPixel(i, j).R;
+                    if (r <= 126)
+                    {
+                        cor = CriarCor(0, 0, 0);
+                        imgnova.SetPixel(i, j, cor);
+                    }
+                    else
+                    {
+                        cor = CriarCor(255, 255, 255);
+                        imgnova.SetPixel(i, j, cor);
+                    }
+                }
             }
-            else
-            {
-                return Color.FromArgb(255, r, g, b);
-            }
+            imgnova.Save("C:/Imagens/novaImagem3.jpg");
+            pictureBox3.Load("C:/Imagens/novaImagem3.jpg");
         }
 
-
+        public void GrayScale()
+        {
+            int coluna = imgnova.Width;
+            int linha = imgnova.Height;
+            Color cor;
+            for (int i = 0; i <= coluna - 1; i++)
+            {
+                for (int j = 0; j <= linha - 1; j++)
+                {
+                    byte r = imgFogao.GetPixel(i, j).R;
+                    byte g = imgFogao.GetPixel(i, j).G;
+                    byte b = imgFogao.GetPixel(i, j).B;
+                    byte gray = (byte)((r * 0.30) + (g * 0.59) + (b * 0.11));
+                    cor = CriarCor(gray, gray, gray);
+                    imgnova.SetPixel(i, j, cor);
+                }
+            }
+            imgnova.Save("C:/Imagens/novaImagem2.jpg");
+            pictureBox2.Load("C:/Imagens/novaImagem2.jpg");
+        }
 
         public void ProcessarImagens()
         {
@@ -53,24 +76,25 @@ namespace Projeto3Bim
             Color cor;
 
 
-            for (byte i = 0; i <= coluna - 1; i++)
+            for (int i = 0; i <= coluna - 1; i++)
             {
-                for (byte j = 0; j <= linha - 1; j++)
+                for (int j = 0; j <= linha - 1; j++)
                 {
                     byte r = imgPanela.GetPixel(i, j).R;
                     byte g = imgPanela.GetPixel(i, j).G;
                     byte b = imgPanela.GetPixel(i, j).B;
-                    if (!(r == 255 & g == 255 && b == 0))
+                    if (!(r > 100 & g > 100 && b < 110))
                     {
                         cor = imgPanela.GetPixel(i, j);
-                        imgnova.SetPixel(i, j, cor);
+                        imgnova.SetPixel(i + 135, j, cor);
                     }
-                    
+
                 }
             }
 
 
             imgnova.Save("C:/Imagens/novaImagem.jpg");
+            pictureBox1.Load("C:/Imagens/novaImagem.jpg");
         }
 
 
@@ -78,6 +102,8 @@ namespace Projeto3Bim
         private void button1_Click(object sender, EventArgs e)
         {
             ProcessarImagens();
+            GrayScale();
+            BinareImage();
         }
     }
 }
